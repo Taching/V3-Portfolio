d1fb979c3726c91c9f1d58ba33c863771bf2d4f5
Takatoshi Wada <takatoshiwada@Takatoshis-MacBook-Pro.local>
2022-02-04 19:48:11 +0800

    Initial Commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import styled from 'styled-components'
+
+export const Footer = () => (
+  <Wrapper>
+    <p>test</p>
+  </Wrapper>
+)
+
+const Wrapper = styled.footer`
+  text-align: center;
+  padding: 142px 0 26px;
+`
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <>
+      <p>test</p>
+    </>
+  )
+}
+
+export default Index
